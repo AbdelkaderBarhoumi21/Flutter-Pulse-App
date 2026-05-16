6b96ae8ddb3aa846adbf616882ab2eca0c932102
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-05-16 19:21:27 +0100

    refactor(api): streamline AppDioClient by removing unnecessary create method

--- lib/core/network/app_dio_client.dart
+++ lib/core/network/app_dio_client.dart
@@ -1,19 +1,13 @@
 import 'package:dio/dio.dart';
 
-
 class AppDioClient {
   AppDioClient._();
-  static final instance = AppDioClient._();
-  Dio create() {
-    final dio = Dio(
-      BaseOptions(
-        connectTimeout: const Duration(seconds: 5),
-        receiveTimeout: const Duration(seconds: 5),
-        headers: {'content-type': 'application/json'},
-      ),
-    );
 
-    dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
-    return dio;
-  }
+  static final Dio instance = Dio(
+    BaseOptions(
+      connectTimeout: const Duration(seconds: 5),
+      receiveTimeout: const Duration(seconds: 5),
+      headers: {'content-type': 'application/json'},
+    ),
+  )..interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
 }
